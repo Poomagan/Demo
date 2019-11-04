11dfa9ea22c4919afcb113f6717e3f1956212c13
Poomagan <32567653+Poomagan@users.noreply.github.com>
2019-11-04 14:55:49 +0530

    Updated clone_repo

--- clone_repo.groovy
+++ clone_repo.groovy
@@ -7,26 +7,13 @@ import hudson.AbortException
 import hudson.console.ModelHyperlinkNote
 import hudson.plugins.git.GitException
 import java.util.logging.Level
-import jenkins.plugins.http_request.ResponseContentSupplier
-import net.sf.json.JSONArray
-import net.sf.json.JSONObject
-import org.jenkinsci.plugins.workflow.cps.CpsClosure2
-import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
-import org.junit.runners.model.MultipleFailureException
-import groovy.json.JsonSlurper
-import groovy.json.JsonOutput
-import groovy.util.Eval
-import java.io.File
-
-// Booleanise Boolean Jenkins Parameters
-DEBUG = DEBUG.toBoolean()
 
 // Other Jenkins parameters:
 GIT_CREDENTIALS = 'c4153dd1-4ef2-44d7-8104-5ef64b2dea47' // The jenkins credentials id to use.
 DEMO_GIT_URL = 'https://github.com/Poomagan/Demo.git' // The repo to checkout from.
 DEMO_2_GIT_URL = 'https://github.com/Poomagan/Demo_2.git'
 
-node(jenkinsAgentNode) {
+node('master') {
 
 // Stage 1
 	stage('Git_Checkout Stage') {
